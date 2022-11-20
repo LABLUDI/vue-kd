bcabd3c6eb64281c733d4026433ba4048f981574
AlexL <chuiflame@gmail.com>
2022-11-20 22:32:24 +0200

    start
    start

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+		<Header />
+		<Main />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Main from './views/Main.vue'
+
+export default {
+	components: {
+		Header,
+		Main,
+	}
+}
+</script>
+
+<style lang="scss">
+@import "./assets/layouts/normalize.module.scss";
+
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+	background: #fff;
+	.container {
+		max-width: 1280px;
+		margin: 0 auto;
+		background: #FFFFFF;
+	}
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,160 @@
+<template>
+	<main>
+		<section className="section-people">
+			<div className="container">
+				<ul className="">
+					<li>
+						<img />
+						<div>
+							<h3>Алексей Миногаров</h3>
+							<span>mi</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Алиса Иванова</h3>
+							<span>al</span>
+							<p>Designer</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Андрей Иванов</h3>
+							<span>iv</span>
+							<p>Designer</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Анна Иванова</h3>
+							<span>an</span>
+							<p>Analyst Team Lead</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Анна Смирнова</h3>
+							<span>sm</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Влада Вейн</h3>
+							<p>Manager</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Дана Хаматова</h3>
+							<p>Manager</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Иван Андреенко</h3>
+							<span>va</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Инга Антонова</h3>
+							<span>ai</span>
+							<p>Designer</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Ирен Адлер</h3>
+							<p>Manager</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Ольга Петрова</h3>
+							<span>po</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Ольга Сергеевна</h3>
+							<span>so</span>
+							<p>Designer</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Павел Волков</h3>
+							<span>vp</span>
+							<p>Art-Director</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Петр Сиша</h3>
+							<span>pe</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Саймон Кит</h3>
+							<p>Team Lead</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Светлана Осипова</h3>
+							<span>os</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Стас Петрич</h3>
+							<span>ps</span>
+							<p>Designer</p>
+						</div>
+					</li>
+					<li>
+						<img />
+						<div>
+							<h3>Улан Капелов</h3>
+							<span>mi</span>
+							<p>Analyst</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
+	</main>
+</template>
+
+<script>
+export default {
+	name: "Main"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
