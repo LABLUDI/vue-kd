5a2fb041f1aec65a2932a2d07432e7457f190ddc
AlexL <chuiflame@gmail.com>
2022-11-24 00:58:42 +0200

    rename
    rename

--- src/components/List.vue
+++ src/components/List.vue
@@ -14,137 +14,132 @@
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av2.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Алиса Иванова</h3>
+							<span class="section-people-profession">al</span>
+							<p class="section-people-profession">Designer</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av3.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Андрей Иванов</h3>
+							<span class="section-people-profession">iv</span>
+							<p class="section-people-profession">Designer</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av4.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Анна Иванова</h3>
+							<span class="section-people-profession">an</span>
+							<p class="section-people-profession">Analyst Team Lead</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av5.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
+							<h3 class="section-people-title">Анна Смирнова</h3>
+							<span class="section-people-profession">sm</span>
 							<p class="section-people-profession">Analyst</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av6.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Влада Вейн</h3>
+							<p class="section-people-profession">Manager</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av7.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Дана Хаматова</h3>
+							<p class="section-people-profession">Manager</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av8.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
+							<h3 class="section-people-title">Иван Андреенко</h3>
+							<span class="section-people-profession">va</span>
 							<p class="section-people-profession">Analyst</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av9.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Инга Антонова</h3>
+							<span class="section-people-profession">ai</span>
+							<p class="section-people-profession">Designer</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av10.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Ирен Адлер</h3>
+							<p class="section-people-profession">Manager</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av11.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
+							<h3 class="section-people-title">Ольга Петрова</h3>
+							<span class="section-people-profession">po</span>
 							<p class="section-people-profession">Analyst</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av12.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Ольга Сергеевна</h3>
+							<span class="section-people-profession">so</span>
+							<p class="section-people-profession">Designer</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av13.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Павел Волков</h3>
+							<span class="section-people-profession">vp</span>
+							<p class="section-people-profession">Art-Director</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av14.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
+							<h3 class="section-people-title">Петр Сиша</h3>
+							<span class="section-people-profession">pe</span>
 							<p class="section-people-profession">Analyst</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av15.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Саймон Кит</h3>
+							<p class="section-people-profession">Team Lead</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av16.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
+							<h3 class="section-people-title">Светлана Осипова</h3>
+							<span class="section-people-profession">os</span>
 							<p class="section-people-profession">Analyst</p>
 						</div>
 					</li>
 					<li class="section-people-item">
 						<img class="section-people-img" src="../img/av17.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Стас Петрич</h3>
+							<span class="section-people-profession">ps</span>
+							<p class="section-people-profession">Designer</p>
 						</div>
 					</li>
 					<li class="section-people-item">
-						<img class="section-people-img" src="../img/av18.webp" />
+						<img class="section-people-img" src="../img/av9.webp" />
 						<div class="section-people-item__box">
-							<h3 class="section-people-title">Алексей Миногаров</h3>
-							<span class="section-people-profession">mi</span>
-							<p class="section-people-profession">Analyst</p>
+							<h3 class="section-people-title">Марго</h3>
+							<p class="section-people-profession">Manager</p>
 						</div>
 					</li>
 				</ul>
